9909b5497e2863454cc764b9daef9bb91b857ecf
Gustavo Higuti <gustavo.france@gmail.com>
2025-07-16 19:22:37 -0300

    scprits JS

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('whatsapp').addEventListener('input', function (e) {
+    let value = e.target.value.replace(/\D/g, '');
+
+    if (value.length > 2) value = `(${value.slice(0, 2)}) ${value.slice(2)}`;
+if (value.length > 10) value = `${value.slice(0, 10)}-${value.slice(10, 14)}`;
+
+e.target.value = value;
+});
